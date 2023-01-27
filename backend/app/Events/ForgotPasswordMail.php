5862623b4fefd185e0e953c87d7de10ebede72dc
adamalbny <adam.albani.t@gmail.com>
2023-01-27 19:37:48 +0700

    Forgot Password

---
+++ backend/app/Events/ForgotPasswordMail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ForgotPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    protected $email;
+    
+    public function __construct( $email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+        ->from(env('MAIL_USERNAME'), env('MAIL_FROM_NAME'))
+        ->subject('APP Doc - Forgot Password')
+        ->view('mails.forgotpassword', [
+            'email' => $this->email
+        ]);
+    }
+}
