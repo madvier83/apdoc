6307abc37d9f60740d7047b7ca3cfa57351332c9
adamalbny <adam.albani.t@gmail.com>
2023-03-28 18:20:29 +0700

    CI/CD Docker

---
+++ backend/app/Console/Commands/DailyWhatsappAppointment.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use App\Notifications\AppointmentWhatsapp;
+
+class DailyWhatsappAppointment extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'appointment:whatsapp';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Whatsapp Appointment';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $datas = App\Models\Appointment::whereDate('appointment_date', Carbon::tomorrow())->get();
+        $setting =  App\Models\Setting::first();
+            foreach($datas as $data){
+                \Notification::route('whatsapp', 'WHATSAPP_SESSION')->notify(new WhatsappAppointment($data->patient->name,
+                                    $data->appointment_date, $data->description,$data->patient->phone,
+                                    $setting->name, $setting->address, $setting->phone));
+            }
+            
+        return response(['success' => 'Messages Appointment Sended!']);
+    }
+}
