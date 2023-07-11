16ab682ddd0161821675a2873e192e3660bf66ea
mcsans <mihsansaepulloh9@gmail.com>
2023-07-11 16:40:04 +0700

    - FIxing Address Sort

--- backend/app/Http/Controllers/AddressController.php
+++ backend/app/Http/Controllers/AddressController.php
@@ -7,7 +7,7 @@ class AddressController extends Controller
     public function index(){
         try {
             $provinces = \Indonesia::allProvinces();
-            return response()->json(['status' => 'success', 'data' => $provinces->sortBy('name')], 200);
+            return response()->json(['status' => 'success', 'data' => $provinces->sortBy('name')->values()->all()], 200);
         } catch (\Throwable $th) {
             return response()->json(['status' => 'error', 'message' => $th->getMessage()], 400);
         }
@@ -15,7 +15,7 @@ class AddressController extends Controller
     public function cities($id){
         try {
             $cities = \Indonesia::findProvince($id, ['cities'])->cities;
-            return response()->json(['status' => 'success', 'data' => $cities->sortBy('name')], 200);
+            return response()->json(['status' => 'success', 'data' => $cities->sortBy('name')->values()->all()], 200);
         } catch (\Throwable $th) {
             return response()->json(['status' => 'error', 'message' => $th->getMessage()], 400);
         }
@@ -23,7 +23,7 @@ class AddressController extends Controller
     public function districts($id){
         try {
             $district = \Indonesia::findCity($id, ['districts'])->districts;
-            return response()->json(['status' => 'success', 'data' => $district->sortBy('name')], 200);
+            return response()->json(['status' => 'success', 'data' => $district->sortBy('name')->values()->all()], 200);
         } catch (\Throwable $th) {
             return response()->json(['status' => 'error', 'message' => $th->getMessage()], 400);
         }
@@ -31,7 +31,7 @@ class AddressController extends Controller
     public function villages($id){
         try {
             $district = \Indonesia::findDistrict($id, ['villages'])->villages;
-            return response()->json(['status' => 'success', 'data' => $district->sortBy('name')], 200);
+            return response()->json(['status' => 'success', 'data' => $district->sortBy('name')->values()->all()], 200);
         } catch (\Throwable $th) {
             return response()->json(['status' => 'error', 'message' => $th->getMessage()], 400);
         }
